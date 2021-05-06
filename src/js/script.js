051902236b1e01ca1398d2ac44c0b53bda8e1692
Emanuele Di Giacomo <emanueledg44@gmail.com>
2021-05-06 21:57:11 +0200

    continuing building landing page

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,32 @@
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+
+// DOM
+const overlay = document.querySelector('.overlay');
+const loginScreen = document.querySelector('.login');
+const loginButton = document.querySelector('.login-btn');
+
+const manipulateView = function (method) {
+  overlay.classList[method]('hidden');
+  loginScreen.classList[method]('hidden');
+};
+
+// Listeners
+loginButton.addEventListener('click', e => {
+  e.preventDefault();
+
+  manipulateView('remove');
+});
+
+overlay.addEventListener('click', _ => {
+  manipulateView('add');
+});
+
+document.addEventListener('keydown', e => {
+  const { key } = e;
+
+  if (key === 'Escape') {
+    manipulateView('add');
+  }
+});
